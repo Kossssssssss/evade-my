bd1642b7068fb3e5c6162c9cfe96ad6701acaaf1
OleksiiKharkov <oleksiykharkov@gmail.com>
2025-08-21 21:55:44 +0300

    config

--- src/configs/location_config.ts
+++ src/configs/location_config.ts
@@ -8,8 +8,8 @@ export interface LocationConfig
 }
 
 export const locations: LocationConfig[] = [
-  { name: 'Easy Forest', enemy_speed: 200, spawn_rate: 2, score_point: 10, background_color: '#228B22' },
-  { name: 'Medium Desert', enemy_speed: 250, spawn_rate: 4, score_point: 20, background_color: '#EDC9AF' },
-  { name: 'Hard Space', enemy_speed: 350, spawn_rate: 5, score_point: 30, background_color: '#000000' }
+  { name: 'Easy Forest'  , enemy_speed: 100, spawn_rate: 1, score_point: 10, background_color: '#228B22' },
+  { name: 'Medium Desert', enemy_speed: 200, spawn_rate: 3, score_point: 20, background_color: '#EDC9AF' },
+  { name: 'Hard Space'   , enemy_speed: 300, spawn_rate: 5, score_point: 30, background_color: '#000000' }
 ];
 
