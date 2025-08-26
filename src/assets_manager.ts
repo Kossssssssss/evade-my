2b811218394529a427309393e3157d997ef36b0d
conny-coder <kostia.shkambula@gmail.com>
2025-08-26 17:35:29 +0300

    little fix

--- src/assets_manager.ts
+++ src/assets_manager.ts
@@ -2,35 +2,37 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils.js';
 
-export class AssetManager {
+export class AssetManager
+{
   private static gltf: any;
   private static textures: Record<string, THREE.Texture> = {};
 
-  static async loadAll(): Promise<void> {
+  static async loadAll(): Promise<void>
+  {
     const loader = new GLTFLoader();
-    const texLoader = new THREE.TextureLoader();
+    const tex_loader = new THREE.TextureLoader();
 
-    // Завантажуємо модельку
-    this.gltf = await new Promise((resolve, reject) => {
-      loader.load('/assets/all.glb', resolve, undefined, reject);
-    });
+    this.gltf = await new Promise( ( resolve, reject ) =>
+    {
+      loader.load( '/assets/all.glb', resolve, undefined, reject );
+    } );
 
-    // Завантажуємо текстури
-    this.textures['green'] = texLoader.load('/assets/green_texture.png');
-    this.textures['purple'] = texLoader.load('/assets/purple_texture.png');
-    this.textures['enemy_normal'] = texLoader.load('/assets/normal_texture.png');
-this.textures['enemy_emissive'] = texLoader.load('/assets/emmisive_texture.png');
+    this.textures['green'] = tex_loader.load( '/assets/green_texture.png' );
+    this.textures['purple'] = tex_loader.load( '/assets/purple_texture.png' );
   }
 
-  static getModel(): THREE.Object3D {
-    return SkeletonUtils.clone(this.gltf.scene);
+  static getModel(): THREE.Object3D
+  {
+    return SkeletonUtils.clone( this.gltf.scene );
   }
 
-  static getTexture(name: string): THREE.Texture {
+  static getTexture( name: string ): THREE.Texture
+  {
     return this.textures[name];
   }
 
-  static getAnimations(): THREE.AnimationClip[] {
+  static getAnimations(): THREE.AnimationClip[]
+  {
     return this.gltf.animations;
   }
 }
